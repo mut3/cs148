3ac628c3efded8200099948b2ad9d43d29998fc6
Will Barnwell <wwbarnwell@gmail.com>
2015-09-07 15:51:57 -0400

    Made finishing touches to website

--- assignment1.0/header.php
+++ assignment1.0/header.php
@@ -5,8 +5,8 @@
             <img alt="calvin and hobbes has influenced my snowman making." src="images/snowmen.jpg">
             <figcaption>Snowmen observing an accident. Mr Watterson made some great comics.</figcaption>
     </figure>
-    <h1>Main title for SITE</h1>
-    <h2>Tag line</h2>
+    <h1>Will's Snowman Table Page</h1>
+    <h2>Tag, you"re it!</h2>
 </header>
 
 <!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
