b681a9782a48e94a3c99dc6e1c1b403b587d9404
Will Barnwell <wwbarnwell@gmail.com>
2015-09-09 13:56:30 -0400

    Added some stuff, fixed security mistake

---
+++ sitemap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+<title>Main Index</title>
+<meta charset="utf-8">
+<meta name="author" content="Will Barnwell wwbarnwell@gmail.com">
+<meta name="description" content="Sitemap of Will Barnwell's CS148 folder">
+
+<!--This is my favicon-->
+<link rel="icon" href="favicon.ico">
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+<body><h1>Main Index</h1>
+
+<h2>
+	<ul>        
+        <li><a href="assignment1.0/index.php">Assignment1.0</a></li>
+        	<ul>
+            	<li><a href="assignment1.0/Assign1Specs.pdf">Spec Sheet</a></li>
+			</ul>
+    </ul>
+</h2>  
+</html>
