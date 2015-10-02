8ded96c78e39cab7ea2cc5d61fd8552c8ae4e0bc
Will Barnwell <wwbarnwell@gmail.com>
2015-10-02 14:04:51 -0400

    commented out debugs

--- misc/friday.php
+++ misc/friday.php
@@ -18,12 +18,16 @@
     $quotes = 0;
     $symbols = 0;
     
-    $thisDatabaseReader->testQuery($query, "", $wheres, $conditions, $quotes, $symbols);
+    //$thisDatabaseReader->testQuery($query, "", $wheres, $conditions, $quotes, $symbols);
     $info2 = $thisDatabaseReader->select($query, "", $wheres, $conditions, $quotes, $symbols);
     $headerFields = array_keys($info2[0]);
-    print_r ('<pre><p>' . $headerFields . '</p></pre>');
+    // echo '<pre><p>';
+    // print_r ($headerFields);
+    // echo '</p></pre>';
     $headerArray = array_filter($headerFields, "is_string");
-    print_r ('<pre><p>' . $headerArray . '</p></pre>');
+    // echo '<pre><p>';
+    // print_r ($headerArray);
+    // echo '</p></pre>';
 
     echo "<h2> Records: " . count($info2) . "</h2>";
     print '<table>';
@@ -32,7 +36,7 @@
     print '<tr class="tblHeaders">';
     foreach ($headerArray as $key) {
         $camelCase = preg_split('/(?=[A-Z])/', substr($key, 3));
-
+        $message = "";
         foreach ($camelCase as $one) {
             $message .= $one . " ";
         }
