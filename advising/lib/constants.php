ccbe181ecea4c3ab7fec6da25b9a4df4ebcd0501
Will Barnwell <wwbarnwell@gmail.com>
2015-10-27 20:02:20 -0400

    set db name

--- advising/lib/constants.php
+++ advising/lib/constants.php
@@ -1,7 +1,7 @@
 <?php
 // print "<p>SQL: " . $sql . "<pre>"; print_r($data); print "</pre></p>";     
 // print "<p>Array:<pre>"; print_r($results); print "</pre></p>";
-define("DATABASE_NAME", strtoupper(get_current_user()) . '_UVM_Courses_Testing');
+define("DATABASE_NAME", strtoupper(get_current_user()) . '_advising');
 
 define("ADMIN_EMAIL", get_current_user() . "@uvm.edu");
 
