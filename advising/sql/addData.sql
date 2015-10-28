2294a42642c29f5ff8374123aa0788febdb518ee
Will Barnwell <wwbarnwell@gmail.com>
2015-10-27 21:11:10 -0400

    spoof

--- advising/sql/addData.sql
+++ advising/sql/addData.sql
@@ -8,10 +8,10 @@ INSERT INTO tbl4yPlan (fnkAdviserNetId, fnkStudentNetId, fldCatalogYear, fldMajo
 ("jlhorton", "wbarnwel", 2013, "Computer Science", "-");
 
 INSERT INTO tblSemesterPlan (fnkPlanId, fldYear, fldTerm, fldDisplayOrder) VALUES
-(SELECT pmkId FROM tbl4yPlan WHERE fnkStudentNetId="wbarnwel", 2013, "Fa", 1);
+(1, 2013, "Fa", 1);
 
 INSERT INTO tblSemesterPlanCourses (fnkPlanId, fnkYear, fnkTerm, fnkCourseId, fldDisplayOrder) VALUES
-(SELECT pmkId FROM tbl4yPlan WHERE fnkStudentNetId="wbarnwel", 2013, "Fa", SELECT pmkId FROM tblCourses WHERE fldDepartment="CS" AND fldNumber=008, 1);
+(1, 2013, "Fa", 1, 1);
 
 INSERT INTO tblCourses (fldDepartment, fldNumber, fldName, fldCredits) VALUES
 ("CS", 008, "Intro to Web Design", "2");
