7eedff0c7a850384f0998d71f091cde5009f123e
Will Barnwell <wwbarnwell@gmail.com>
2015-12-07 18:43:53 -0500

    added account page

---
+++ assignment10/account.php
@@ -0,0 +1,14 @@
+<?php
+	// include top
+	include 'top.php';
+
+	/* 
+		change or set user preferences
+		form autofills current values if they exist
+	*/
+?>
+
+<?php
+	// include top
+	include 'bottom.php';
+?>
