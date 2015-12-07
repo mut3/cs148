afe4a20bc3b719cc8409fcd9bb23e23fb67c262b
Will Barnwell <wwbarnwell@gmail.com>
2015-12-07 18:34:42 -0500

    added dummy text

--- assignment10/about.php
+++ assignment10/about.php
@@ -5,6 +5,11 @@
 	/* 
 		Tell people what its all about
 	*/
+?>
+<article>
+	Donec dignissim odio augue, ac sollicitudin mi condimentum sollicitudin. Fusce pellentesque a arcu vel tempus. Sed vitae dolor gravida, gravida nisi vel, elementum massa. Sed elementum eu dolor nec commodo. Phasellus sem diam, porta non lacus nec, consequat cursus sapien. Sed sit amet tempor turpis, at tempor neque. Fusce at magna porttitor nibh accumsan lacinia non vel eros. Curabitur non lorem congue, imperdiet risus a, facilisis dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque tortor sem, elementum non blandit nec, vestibulum sed nisi. In nulla sapien, elementum ac lacinia nec, aliquam non purus. Donec a urna semper, ornare purus vitae, fermentum tortor. Aenean ut faucibus nisl. Ut ut suscipit erat. Aenean quis venenatis mauris, ac luctus est.
+</article>
+<?php
 	// include top
 	include 'bottom.php';
 ?>
