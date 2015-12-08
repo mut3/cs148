f669750084a104452c2b5aadf58f28593374d769
Will Barnwell <wwbarnwell@gmail.com>
2015-12-08 01:09:19 -0500

    added key constraints

--- assignment10/sql/rebuildDB.sql
+++ assignment10/sql/rebuildDB.sql
@@ -4,13 +4,16 @@ CREATE TABLE tblUser
 	pmkUserId int NOT NULL AUTO_INCREMENT,
 	fldUsername varchar(8) NOT NULL,
 	fldEmail varchar(255),
-	fldAdmin boolean
+	fldAdmin boolean,
+	PRIMARY KEY(pmkUserId)
 );
 CREATE TABLE tblItem
 (
 	pmkItemId int NOT NULL AUTO_INCREMENT,
 	fnkOwnerId int NOT NULL,
-	fldItemName varchar(255) NOT NULL
+	fldItemName varchar(255) NOT NULL,
+	PRIMARY KEY(pmkItemId),
+	CONSTRAINT fk_OwnerId FOREIGN KEY (fnkOwnerId) REFERENCES tblUser(pmkUserId)
 );
 
 -- INSERT INTO tblUser (fldUsername, fldEmail, fldAdmin) VALUES
