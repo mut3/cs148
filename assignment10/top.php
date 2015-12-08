4499d3bce2814211387af041f3d46263fea4162f
Will Barnwell <wwbarnwell@gmail.com>
2015-12-08 17:40:09 -0500

    testing

--- assignment10/top.php
+++ assignment10/top.php
@@ -162,4 +162,44 @@ require_once('lib/custom-functions.php');
 	print '<body id="' . $path_parts['filename'] . '">';
 	include "header.php";
 	include "nav.php";
+	$headerFields = array_keys($userRecord[0]);
+    // echo '<pre><p>';
+    // print_r ($headerFields);
+    // echo '</p></pre>';
+    $headerArray = array_filter($headerFields, "is_string");
+    // echo '<pre><p>';
+    // print_r ($headerArray);
+    // echo '</p></pre>';
+
+    echo "<h2> Records: " . count($userRecord) . "</h2>";
+    print '<table>';
+
+    //header block
+    print '<tr class="tblHeaders">';
+    foreach ($headerArray as $key) {
+        $camelCase = preg_split('/(?=[A-Z])/', substr($key, 3));
+        $message = "";
+        foreach ($camelCase as $one) {
+            $message .= $one . " ";
+        }
+        print '<th>' . $message . '</th>';
+    }
+    print '</tr>';
+
+    //data printed to table
+    $highlight = 0; // used to highlight alternate rows
+    foreach ($userRecord as $rec) {
+        $highlight++;
+        if ($highlight % 2 != 0) {
+            $style = ' odd ';
+        } else {
+            $style = ' even ';
+        }
+        print '<tr class="' . $style . '">';
+        for ($i = 0; $i < $columns; $i++) {
+            print '<td>' . $rec[$i] . '</td>';
+        }
+        print '</tr>';
+    }
+    print '</table>';
 ?>
