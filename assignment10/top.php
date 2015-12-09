81f75d0c47a2fae0b6320a3d9aa2ca47cae8a67a
Will Barnwell <wwbarnwell@gmail.com>
2015-12-09 12:59:58 -0500

    Wooooo bootstrap

--- assignment10/top.php
+++ assignment10/top.php
@@ -15,7 +15,14 @@ require_once('lib/custom-functions.php');
 	<!--[if lt IE 9]>
 	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
 	<![endif]-->
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="css/base.css" type="text/css" media="screen">
 
 	<?php
