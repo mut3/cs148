c34e109a96105233d69ad73bb7911adb51902712
Will Barnwell <wwbarnwell@gmail.com>
2015-12-09 13:40:33 -0500

    AHHHHHHHHHHH

--- assignment10/use.php
+++ assignment10/use.php
@@ -10,6 +10,10 @@
 			If sufficient amounts are had for all “Apply Recipe” button exists
 				Updates records and reports back updated levels of all items updated
 	*/
+?>
+	<p>This page will, in later versions, have a tool to apply deductions en masse to supplies in your list. This will hopefully make keeping this tool up to date with your cooking easy.</p>
+	<a href="https://uvmcscrew.com">While you are waiting, check out the UVM CSCREW.</a>
+<?php
 	// include top
 	include 'bottom.php';
 ?>
