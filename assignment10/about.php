8bf704481e371cf57b5d66a1d04fcd6f50ae6af2
Will Barnwell <wwbarnwell@gmail.com>
2015-12-09 12:31:49 -0500

    created about

--- assignment10/about.php
+++ assignment10/about.php
@@ -7,7 +7,7 @@
 	*/
 ?>
 <article>
-	Donec dignissim odio augue, ac sollicitudin mi condimentum sollicitudin. Fusce pellentesque a arcu vel tempus. Sed vitae dolor gravida, gravida nisi vel, elementum massa. Sed elementum eu dolor nec commodo. Phasellus sem diam, porta non lacus nec, consequat cursus sapien. Sed sit amet tempor turpis, at tempor neque. Fusce at magna porttitor nibh accumsan lacinia non vel eros. Curabitur non lorem congue, imperdiet risus a, facilisis dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque tortor sem, elementum non blandit nec, vestibulum sed nisi. In nulla sapien, elementum ac lacinia nec, aliquam non purus. Donec a urna semper, ornare purus vitae, fermentum tortor. Aenean ut faucibus nisl. Ut ut suscipit erat. Aenean quis venenatis mauris, ac luctus est.
+	<p>Welcome to Pantry Raid v0.1 Early-Access-Pre-Alpha (Gold Tier or above backers only). This is a 20-hour hack of php and SQL to provide a proof of concept for a personal project. The goal of Pantry Raid is to assist the busy cook/chef/baker/kitchen-denizen keep track of their supply levels. Once the base app is running with the functionality it deserves, I plan on creating an API and riding the Internet of Things hype-train. </p>
 </article>
 <?php
 	// include top
