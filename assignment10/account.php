c10217b9f59dc10ae10f4990f5e902b8980725d0
Will Barnwell <wwbarnwell@gmail.com>
2015-12-08 19:47:52 -0500

    added sections of php

--- assignment10/account.php
+++ assignment10/account.php
@@ -1,14 +1,30 @@
-<?php
+<?php //top
 	// include top
 	include 'top.php';
 
+
 	/* 
-		change or set user preferences
-		form autofills current values if they exist
+	change or set user preferences
+	form autofills current values if they exist
 	*/
-	echo "<p>Account settings of user: $username</p>";
 ?>
-<?php
-	// include top
+<?php // form engine
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Variable initialization done in top
+	//if there are values in the post
+
+
+?>
+<?php // Page Content & Form
+	echo "<p>Account settings of user: $username</p>";
+	if ($newUser) {
+		echo "<h2>New Account Setup</h2>";
+		echo "<p>Please setup your account before attempting to use the site</p>"
+	} else {
+		echo "<h2>Account Settings</h2>";
+	}
+?>
+<?php //foot
+	// include bottom
 	include 'bottom.php';
 ?>
